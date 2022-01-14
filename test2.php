1c8f9a357837dd6b179385f9dfb2853345e36359
kotross <kotross@users.noreply.github.com>
2022-01-14 20:53:07 +0300

    test2.php

---
+++ test2.php
@@ -0,0 +1,27 @@
+/*
+redis|mysql|postgres|amqp|mongodb|elasticsearch|postgresql
+passwd|passphrase|password|secret_key|api_key|secretkey|apikey|securitytoken|bearer|cardnumber|clientSecret
+aws_access_key_id|aws_secret_access_key
+public String secretKey;
+public MyThread(String secretKey) {
+    this.secretKey = secretKey;
+*/
+
+
+postgres
+mysql
+redis
+amqp
+mongodb
+elasticsearch
+postgresql
+
+
+axa app
+axa assistance
+axa health insurance
+axa insurance
+axa life insurance
+axa login
+axa online
+axainsurance
