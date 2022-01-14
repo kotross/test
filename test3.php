184cc9fb60ff9443942d7f11a8a73d4baa00c2c6
kotross <kotross@users.noreply.github.com>
2022-01-14 20:58:35 +0300

    Create test3.php

---
+++ test3.php
@@ -0,0 +1,10 @@
+/*
+redis|mysql|postgres|amqp|mongodb|elasticsearch|postgresql
+passwd|passphrase|password|secret_key|api_key|secretkey|apikey|securitytoken|bearer|cardnumber|clientSecret
+aws_access_key_id|aws_secret_access_key
+public String secretKey;
+public MyThread(String secretKey) {
+    this.secretKey = secretKey;
+*/
+
+axainsurance
