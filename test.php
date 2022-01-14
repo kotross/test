ef54334b4238a8e74751ef7f8f5d34cb10e87536
kotross <kotross@users.noreply.github.com>
2022-01-14 19:41:29 +0300

    Update test.php

--- test.php
+++ test.php
@@ -5,6 +5,12 @@ google
 yandex
 
 /*
+redis|mysql|postgres|amqp|mongodb|elasticsearch|postgresql
+
+passwd|passphrase|password|secret_key|api_key|secretkey|apikey|securitytoken|bearer|cardnumber|clientSecret
+
+aws_access_key_id|aws_secret_access_key
+
 public String secretKey;
 public MyThread(String secretKey) {
     this.secretKey = secretKey;
