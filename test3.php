4fe8f263d527360bd12beedf283889a9398856a3
kotross <kotross@users.noreply.github.com>
2024-09-25 17:13:45 +0300

    Update test3.php

--- test3.php
+++ test3.php
@@ -8,3 +8,12 @@ public MyThread(String secretKey) {
 */
 
 axainsurance
+/*
+Создай сам ))
+Там такой текст был
+[default]
+access_key = 184bed11b9ce0e73f219
+secret_key = XTxA0WHOKODkiDiu1DImx4IpOZkxnTA5cTODbxgg
+host_base = s3-msk.tinkoff.ru
+host_bucket = %(bucket)s.s3-msk.tinkoff.ru
+*/
