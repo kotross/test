014bf08a42182399306605ffb8339c36888cc506
kotross <kotross@users.noreply.github.com>
2024-09-25 17:14:31 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,56 +1,3 @@
-111 kitkat 222
-
-google
-
-yandex
-
-/*
-redis|mysql|postgres|amqp|mongodb|elasticsearch|postgresql
-
-passwd|passphrase|password|secret_key|api_key|secretkey|apikey|securitytoken|bearer|cardnumber|clientSecret
-
-aws_access_key_id|aws_secret_access_key
-
-public String secretKey;
-public MyThread(String secretKey) {
-    this.secretKey = secretKey;
-*/
-
-www.sberbank.ru
-sberbank.ru
-test.test.sberbank.ru
-
-217.116.48.210
-52.136.254.247
-
-sber
-sberbank
-сбер
-сбербанк
-
-fewfewfewfewfwefew:fewfewfewfewfffewfew
-
-postgres
-mysql
-redis
-amqp
-mongodb
-elasticsearch
-postgresql
-
-
-axa life insurance
-
-axa app
-axa assistance
-axa health insurance
-axa life insurance
-axa login
-axa online
-
-
-
-/*
 Создай сам ))
 Там такой текст был
 [default]
@@ -58,4 +5,3 @@ access_key = 184bed11b9ce0e73f219
 secret_key = XTxA0WHOKODkiDiu1DImx4IpOZkxnTA5cTODbxgg
 host_base = s3-msk.tinkoff.ru
 host_bucket = %(bucket)s.s3-msk.tinkoff.ru
-*/
