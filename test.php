1a459ddbaa323f4e241d2d373538b59c6b246d18
kotross <kotross@users.noreply.github.com>
2025-01-21 14:52:18 +0300

    Update test.php

--- test.php
+++ test.php
@@ -5,3 +5,11 @@ access_key = 184bed11b9ce0e73f219
 secret_key = XTxA0WHOKODkiDiu1DImx4IpOZkxnTA5cTODbxgg
 host_base = s3-msk.tinkoff.ru
 host_bucket = %(bucket)s.s3-msk.tinkoff.ru
+
+
+api_key = "UWjSzy23711328951174"
+API_KEY = "f493235022f117443717201d55ff1d93"
+API_KEY = 'AIzaSyA8q8h_p-mniKAVyH0OmqQwyLYNYk6PCSI'
+api_key = '1f96b26e-8764-4c1a-8a01-fb1046ceb1b6'
+
+
